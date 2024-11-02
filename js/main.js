760331b413c1b7287ee1ba8b434ce53c2321a7af
Stage09 <ismoil281279@gmail.com>
2024-11-02 16:55:27 +0500

    finished

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// const d = prompt()
+
+// 1 masala
+
+// for(let d = 1; d <= 20; d++){
+//     if(d % 2 !=0){
+//         console.log(d)
+//     }
+// }
+
+// -------------------------------
+
+// 2 masala
+
+// for(let d = 1; d <= 20; d++){
+//     if(d % 2 ==0){
+//         console.log(d)
+//     }
+// }
+
+// ------------------------------
+
+// 3 masala
+
+// let n = prompt("Son kiriting")
+// for(let i = n; i >= 1; i--){
+//     console.log(i)
+// }
+
+// --------------------------------
+
+// 4 masala
+
+// let a = prompt("a-son kiriting") - 0
+// let b = prompt("b-son kiriting") - 0
+
+// for(let i = a; i <= b;i++){
+//     console.log(i)
+// }
+
+// --------------------------------
+
+// ----------------------------
+
+// 6-masala
+
+// let a = prompt("a-son kiriting") - 0
+// let b = prompt("b-son kiriting") - 0
+// let sum = 0
+// for(let i = a; i <= b;i++){
+    //     sum = sum + 1
+    // }
+    // console.log(sum)
+    
+// ----------------------------
+    
+// 7-masala
+
+// let sum = 1
+// for(let i = 5; i <= 10;i++){
+//      sum = sum * i
+// }
+//     console.log(sum)
+
+// --------------------------
+
+// 10-masala
+
+// let sum = prompt("Narx kiriting")
+// let n = prompt("N kiriting")
+
+// for(let i = 0.1; i<= n; i+=0.1){
+//     console.log(Number(i.toFixed(1)) * sum + "sum")
+// }
